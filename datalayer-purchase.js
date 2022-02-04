0678209f0d812520794838f38c3eb6568406b096
Erman Küplü <53745475+ekuplu@users.noreply.github.com>
2022-02-04 18:15:33 +0200

    Create datalayer-purchase.js
    We are adding our basic Shopify purchase data layer into Github.

---
+++ datalayer-purchase.js
@@ -0,0 +1,19 @@
+{% if first_time_accessed %}<script>                               
+	window.dataLayer = window.dataLayer || [];                                            
+	var shipping_price = '{{shipping_price | money_without_currency }}';
+	shipping_price  = shipping_price.replace(",", ".");
+	var total_price = '{{total_price | money_without_currency }}';
+	total_price  = total_price.replace(",", ".");
+	var tax_price = '{{tax_price | money_without_currency }}';
+	tax_price  = tax_price.replace(",", ".");
+	window.dataLayer.push({
+	'page_type': 'purchase',
+	'event': 'analyzify_purchase',
+	'currency': "{{ shop.currency }}",
+	'totalValue': total_price,
+	'shipping': shipping_price,
+	'tax': tax_price,
+	'payment_type': '{{order.transactions[0].gateway}}',
+	'transaction_id': "{{order.name}}",
+	});</script> 
+	{% endif %}
