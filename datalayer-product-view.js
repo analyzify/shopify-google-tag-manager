f19f8b89ddaa0d387e14241f02135c9940833019
Erman Küplü <53745475+ekuplu@users.noreply.github.com>
2022-02-19 07:43:02 +0200

    Create datalayer-product-view.js

---
+++ datalayer-product-view.js
@@ -0,0 +1,17 @@
+   window.analyzifyProductPageHandlePublic = function(){
+        var productName = "{{ product.title | remove: "'" | remove: '"' }}";
+        var productId = "{{ product.id }}";
+        var productPrice = "{{ product.variants.first.price | times: 0.01 }}";
+        var productSku = "{{ product.selected_or_first_available_variant.sku | remove: "'" | remove: '"' }}";
+        var productCollection = "{{ product.collections.last.title | remove: "'" | remove: '"' }}";
+        
+        window.dataLayer.push({
+          event: 'analyzify_productDetail',
+          name: productName,
+          id: productId,
+          price: productPrice,
+          currency: "{{ shop.currency }}",
+          sku: productSku,
+          category: productCollection,
+          variant_id: "{{ product.selected_variant.id | default: product.variants[0].id }}"
+        });
