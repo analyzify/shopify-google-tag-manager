4656033277ab3af43d4e72fc340fc2a2fab223f1
Erman Küplü <53745475+ekuplu@users.noreply.github.com>
2022-03-08 10:18:49 +0200

    Update datalayer-product-view.js

--- datalayer-product-view.js
+++ datalayer-product-view.js
@@ -1,26 +1,31 @@
-	<script>    
-	window.dataLayer = window.dataLayer || []; 
-	window.analyzifyProductPageHandlePublic = function(){
-        var productName = "{{ product.title | remove: "'" | remove: '"' }}";
-        var productId = "{{ product.id }}";
-        var productPrice = "{{ product.variants.first.price | times: 0.01 }}";
-        var productSku = "{{ product.selected_or_first_available_variant.sku | remove: "'" | remove: '"' }}";
-        var productCollection = "{{ product.collections.last.title | remove: "'" | remove: '"' }}";
-        
-        window.dataLayer.push({
-          event: 'analyzify_productDetail',
-          name: productName,
-          id: productId,
-          price: productPrice,
-          currency: "{{ shop.currency }}",
-          sku: productSku,
-          category: productCollection,
-          variant_id: "{{ product.selected_variant.id | default: product.variants[0].id }}"
-        });
-	      {% case template_name %}
-       
-        {% when 'product' %}
-          analyzifyProductPageHandlePublic()
+<script type="text/javascript">
+  window.dataLayer = window.dataLayer || [];
 
-      {% endcase %}
-	</script>                               
+  window.appStart = function(){
+    {% assign template_name = template.name %} 
+
+    window.productPageHandle = function(){
+      var productName = "{{ product.title | remove: "'" | remove: '"' }}";
+      var productId = "{{ product.id }}";
+      var productPrice = "{{ product.price | money_without_currency }}";
+      var productBrand = "{{ product.vendor | remove: "'" | remove: '"' }}";
+      var productCollection = "{{ product.collections.first.title | remove: "'" | remove: '"' }}"
+
+      window.dataLayer.push({
+        event: 'ee_productDetail',
+        name: productName,
+        id: productId,
+        price: productPrice,
+        brand: productBrand,
+        category: productCollection,
+      });
+    };
+
+    {% case template_name %}
+    {% when 'product' %}
+    	productPageHandle()
+    {% endcase %}
+  }
+
+  appStart();
+</script>
